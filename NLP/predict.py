43e9e348847cde9eddf6ee5885dd3d3afd396c3d
Tagai Nao <tagainao@icloud.com>
2020-10-16 19:06:31 +0900

    Revert "Merge pull request #8 from Ryunosuke-Ikeda/origin/feature/ikeda/NLP"
    This reverts commit e42b19a0334ea803c3803940396bd0f52f09e1a6, reversing
    changes made to cc988d2b88569eb6e0986e5cb60e05ab585d7240.

--- NLP/predict.py
+++ NLP/predict.py
@@ -7,61 +7,62 @@ from sklearn.feature_extraction.text import CountVectorizer
 from preprocessings.tokenizer import JanomeTokenizer
 from preprocessings.normalizer import normalize
 
-input_task1=''
 
 
-# janomeでトークナイズ
-# BOWで文をベクトル化
-janome = JanomeTokenizer()
-def tokenize(word):
-    tokens = [normalize(janome.surface(token)) for token in janome.tokenize(word)]
-    return " ".join(tokens)
+input_task1='資格の勉強'
+
+def predict(input_task1):
+
+  # janomeでトークナイズ
+  # BOWで文をベクトル化
+  janome = JanomeTokenizer()
+  def tokenize(word):
+      tokens = [normalize(janome.surface(token)) for token in janome.tokenize(word)]
+      return " ".join(tokens)
 
 
-docs  = [input_task1]
-df = pd.DataFrame(data = { 'docs': docs})
+  docs  = [input_task1]
+  df = pd.DataFrame(data = { 'docs': docs})
 
-#print(df)
-df['docs'] = df['docs'].apply(tokenize)
-    
-#学習済みbowの呼び出し。
-#import pickle
-with open('model/count.pickle', mode='rb') as fp:
-  count = pickle.load(fp)
-#fit_transformすると再学習しちゃうからあかん。
-X_count = count.transform(df['docs'].values)
+  #print(df)
+  df['docs'] = df['docs'].apply(tokenize)
+      
+  #学習済みbowの呼び出し。
+  #import pickle
+  with open('model/count.pickle', mode='rb') as fp:
+    count = pickle.load(fp)
+  #fit_transformすると再学習しちゃうからあかん。
+  X_test = count.transform(df['docs'].values)
 
-X_test=X_count
+  #学習済みデータの読み出し 
+  with open('model/model.pickle', mode='rb') as fp:
+    clf = pickle.load(fp)
 
+  #推論
+  pred=clf.predict_log_proba(X_test)
 
-#学習済みデータの読み出し 
-with open('model/model.pickle', mode='rb') as fp:
-  clf = pickle.load(fp)
+  category = {
+      'kadai': 0,
+      'MTG':1,
+      'training':2,
+      'buy':3,
+      'contact':4,
+      'programming':5,
+    }
 
-print(clf.predict(X_test))
-
-pred=clf.predict_log_proba(X_test)
-
-category = {
-    'study': 0,
-    'work':1,
-    'training':2,
-    'buy':3,
-    'contact':4,
-    'programming':5,
-    'kaji':6,
-    
-  }
-#逆引き
-def inverse_lookup(d, x):
-    for k,v in d.items():
-        if x == v:
-            return k
+  #逆引き
+  def inverse_lookup(d, x):
+      for k,v in d.items():
+          if x == v:
+              return k
 
 
+  if pred.max()<-1:
+    return 'other'
+    #print('else')
+  else:
+    return inverse_lookup(category,pred.argmax())
+    #print(inverse_lookup(category,pred.argmax()))
 
-print(pred)
-if pred.max()<-1.1:
-  print('else')
-else:
-  print(inverse_lookup(category,pred.argmax()))
+
+print(predict(input_task1))
