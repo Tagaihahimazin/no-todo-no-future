ca0456efc218463585e8e2a76ca082da2f250c6f
ryunosuke <ryunosuke0305@gmail.com>
2020-10-17 05:44:14 +0900

    MODEL書き出し

--- testpredict/NLP/predict.py
+++ testpredict/NLP/predict.py
@@ -6,17 +6,63 @@ import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from .preprocessings.tokenizer import JanomeTokenizer
 from .preprocessings.normalizer import normalize
+from testpredict.models import load_NLP
+from testpredict.models import load_NLP2
+import base64
+import json
 
 def load_model_file():
+  '''
+  
   #import pickle
-  with open('testpredict/NLP/model/count.pickle', mode='rb') as fp:
-    count = pickle.load(fp)
+  with open('testpredict/NLP/model/count.pickle', mode='rb') as fp_count:
+    bow = pickle.load(fp_count)
 
   #学習済みデータの読み出し 
-  with open('testpredict/NLP/model/model.pickle', mode='rb') as fp:
-    clf = pickle.load(fp)
+  with open('testpredict/NLP/model/model.pickle', mode='rb') as fp_clf:
+    model = pickle.load(fp_clf)
+  '''
+  d_count = {}
+  with open("testpredict/NLP/model/count.json") as f:
+    d_count = json.load(f)
+  count = d_count["pickle"]
+
+  #count = pickle.loads(base64.b64decode(s.encode()))
+
+  d_clf = {}
+  with open("testpredict/NLP/model/model.json") as f:
+    d_clf = json.load(f)
+  clf = d_clf["pickle"]
+
+  #clf = pickle.loads(base64.b64decode(s.encode()))
+  
+  #return  bow,model
+  #return fp_count,fp_clf
   return count,clf
 
+def upload_db():
+  
+  #書き込み
+  count,clf=load_model_file()
+  
+
+  #count_by= pickle.dumps(count)
+  #clf_by= pickle.dumps(clf)
+  #count_str=base64.b64encode(pickle.dumps(count)).decode("utf-8")
+  #clf_str=base64.b64encode(pickle.dumps(clf)).decode("utf-8")
+  
+
+  #z=load_NLP(NLP_bow=count, NLP_clf=clf)
+
+  z=load_NLP()
+  z.NLP_bow=count
+  z.save()
+
+  q=load_NLP2()
+  q.NLP_clf=clf
+  q.save()
+  
+  print('ok')
 
 
 
@@ -41,9 +87,37 @@ def predict(input_task1='資格の勉強',debug=False):
 
   import pickle
   if debug:
-    print('test')
-  else:
+    
     count,clf=load_model_file()
+  else:
+    print('ok')
+    #upload_db()
+    
+    data1=load_NLP.objects.get(pk=1)
+    bow={}
+    bow=data1.NLP_bow
+    data2=load_NLP2.objects.get(pk=1)
+    model={}
+    model=data2.NLP_clf
+
+    #print("aaaaaaaaaaaaaaaaaaaaaaaaa")
+    #print(model)
+
+
+
+    #count= pickle.load(bow)
+    #clf= pickle.load(model)
+
+    #clf = {}
+    clf = model["pickle"]
+    clf = pickle.loads(base64.b64decode(clf.encode()))
+
+    #count = {}
+    count = bow["pickle"]
+    count = pickle.loads(base64.b64decode(count.encode()))
+    
+
+ 
 
   #fit_transformすると再学習しちゃうからあかん。
   X_test = count.transform(df['docs'].values)
@@ -59,6 +133,10 @@ def predict(input_task1='資格の勉強',debug=False):
   with open('testpredict/NLP/model/model.pickle', mode='rb') as fp:
     clf = pickle.load(fp)
   '''
+ 
+
+
+
 
   #推論
   pred=clf.predict_log_proba(X_test)
--- testpredict/models.py
+++ testpredict/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from picklefield.fields import PickledObjectField
 
+from django.contrib.postgres.fields import JSONField
+
+
+#from django.contrib.postgres.fields import JSONField
+#from django.contrib.postgres.fields import JSONField
 # Create your models here.
 
 class Taskclassification(models.Model):
@@ -8,3 +14,26 @@ class Taskclassification(models.Model):
 
     def __str__(self):
         return self.todo_text
+
+class load_NLP(models.Model):
+    #NLP_bow=models.BinaryField()
+    #NLP_clf=models.BinaryField()
+    NLP_bow=JSONField()
+    #NLP_clf=models.JSONField()
+
+    #NLP_bow=PickledObjectField()
+    #NLP_clf=PickledObjectField()
+
+    #NLP_bow=models.TextField(max_length=30000)
+
+    def __str__(self):
+        #return '{},{}'.format(self.NLP_bow,self.NLP_clf)
+        return self.NLP_bow
+
+
+class load_NLP2(models.Model):
+    NLP_clf=JSONField()
+    #NLP_clf=models.TextField(max_length=30000)
+
+    def __str__(self):
+        return self.NLP_clf
