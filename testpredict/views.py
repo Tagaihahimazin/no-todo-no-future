d2d10720d4cb42f575fe9cf9fd3ff20f8060e519
tagaihahimazin <tagainao@icloud.com>
2020-10-18 01:26:46 +0900

    setting発見

--- testpredict/views.py
+++ testpredict/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 from .NLP import helloworld
